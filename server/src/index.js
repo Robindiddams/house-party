193ebf2de3bfc3ebc4ef67a85835a9a0548b0040
Robin Diddams <robindiddams@gmail.com>
2018-03-17 15:03:50 -0400

    enviornment done

---
+++ server/src/index.js
@@ -0,0 +1,36 @@
+// const express = require('express');
+// const path = require('path');
+import path from 'path';
+import express from 'express';
+// const generatePassword = require('password-generator');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../../web/build')));
+
+// Put all API endpoints under '/api'
+app.get('/api/passwords', (req, res) => {
+	const count = 5;
+
+	// Generate some passwords
+	const passwords = Array.from(Array(count).keys()).map(i =>
+		'beans' + i
+	);
+
+	// Return them as json
+	res.json(passwords);
+
+	console.log(`Sent ${count} passwords`);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname+'../../web/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);
